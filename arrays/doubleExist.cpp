4e4242b6f0a67439e0f548da71767ce1f33769d3
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-09 12:01:26 +0530

    Update doubleExist.cpp

--- arrays/doubleExist.cpp
+++ arrays/doubleExist.cpp
@@ -1,13 +1,13 @@
 // https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3250/
 
+// [-2,0,10,-19,4,6,-8]
+
 // bool checkIfExist(vector<int>& arr) {
-//         sort(arr.begin(),arr.end());
-        
+//         sort(arr.begin(),arr.end());  
 //         int l =0;
 //         int h= arr.size()-1;
 //         int m = l + ((h-l)/2);
 //         while(l<=h){
-            
 //             if((2*l)==m){
 //                 return true;
 //             }else if((2*l)<m){
@@ -15,31 +15,54 @@
 //             }else{
 //                 l=m+1;
 //             }
-            
 //             m = l + ((h-l)/2);
 //         }
-        
 //         return false;
 //     }
 
 
 // bool checkIfExist(vector<int>& arr) {
 //      for(int i=0;i<arr.size();i++){
-            
 //              for(int j=0;j<arr.size();j++){
-                
 //                  if(arr[j]==0){
 //                      continue;
 //                  }
 //                 if((arr[j]*2)==arr[i]){
 //                     return true;
 //                 }
-            
 //             }
-        
-            
 //         }
-        
 //         return false;
 // }
 
+
+//     bool checkIfExist(vector<int>& arr) {
+//         int zeCount = 0;
+//         for(int i=0;i<arr.size();i++){
+//             if(arr[i]==0){
+//                      zeCount++;
+//             }else if(zeCount){
+//                 continue;
+//             }else if(zeCount==2){
+//                  return true;
+//             }
+//             for(int j=0;j<arr.size();j++){
+//                 if((arr[j]*2)==arr[i]){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+
+
+//  bool checkIfExist(vector<int>& arr) {
+//  for(int i = 0; i < arr.size(); i++){
+//             auto it = find(arr.begin(), arr.end(), arr[i]*2);
+//             if(it != arr.end() && it != arr.begin()+i){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
