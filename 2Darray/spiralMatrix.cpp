2c401a9506be17903e9e8d7850ccf873307047a9
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-10 18:16:17 +0530

    Create spiralMatrix.cpp

---
+++ 2Darray/spiralMatrix.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/spiral-matrix/
+
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        //rows = vect.size();
+        // col = vect.size(row number);
+        
+        vector<int> ans;
+        
+        int top =0,left =0;
+        int bottom = matrix.size()-1;
+        int right = matrix[0].size()-1;
+        int dir =0;
+        
+        while(top<=bottom && left<=right){
+            if(dir==0){
+                for(int i = left;i<=right;i++){
+                    ans.push_back(matrix[top][i]);
+                }
+                top++;
+            }else if(dir==1){
+                for(int i = top;i<=bottom;i++){
+                     ans.push_back(matrix[i][right]);
+                }
+                right--;
+            }else if(dir==2){
+                for(int i = right;i>=left;i--){
+                     ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }else if(dir==3){
+                for(int i = bottom;i<=top;i--){
+                     ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+            
+            dir = (dir+1)%4;
+        }
+        
+        
+        return ans;
+        
+    }
+};
