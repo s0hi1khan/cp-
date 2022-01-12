b6a14a87d6114c606a1795c2796a4d1e1b0a4c57
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-12 12:15:20 +0530

    Update DuplicateNumber.cpp

--- arrays/DuplicateNumber.cpp
+++ arrays/DuplicateNumber.cpp
@@ -1,16 +1,35 @@
 // https://leetcode.com/problems/find-the-duplicate-number/ 
- 
-  int findDuplicate(vector<int>& nums) {
- 
- int ans = 0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-                if(nums[i]==nums[i+1]){
-                        return nums[i];
-                }else{
-                        ans = ans ^ i ^ nums[i];
-                }
-            }
-        return ans; 
+
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // int ans = 0;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size();i++){
+        //         if(nums[i]==nums[i+1]){
+        //                 return nums[i];
+        //         }else{
+        //                 ans = ans ^ i ^ nums[i];
+        //         }
+        //     }
+        // return ans; 
         
+        int n = nums.size();
+        vector<int> ans(n+1 , 0);
+        
+        for(int i =0; i<n;i++){
+            
+            if(ans[nums[i]]!=0){
+                return nums[i];
+            }else{
+                ans[nums[i]]++;
+            }
+            
+            
         }
+        return -1;
+        
+    }
+};
