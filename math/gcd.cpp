f23266bac1288c1a249c0b09ffc963607a3715bd
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-12 17:20:52 +0530

    Update gcd.cpp

--- math/gcd.cpp
+++ math/gcd.cpp
@@ -1,3 +1,10 @@
+//  https://www.codingninjas.com/blog/2020/07/25/explained-euclids-gcd-algorithm/
+
+// ans = gcd(a,b) = gcd( a-b , b){a<=b}  =  gcd(a&b , b)  =  gcd(ans , 0) ;
+// gcd(a,b) * lcm(a,b) = a*b;
+
+
+
 int gcd(int a , int b){
   
   if(a==0){return b;}
