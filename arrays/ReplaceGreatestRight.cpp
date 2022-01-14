7c7276e2ef1722f1fa2b0d1556b24e939546fcd5
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-14 15:03:57 +0530

    Update ReplaceGreatestRight.cpp

--- arrays/ReplaceGreatestRight.cpp
+++ arrays/ReplaceGreatestRight.cpp
@@ -22,15 +22,34 @@ public:
     
         
         
-         int n = arr.size();
-        if (n == 0) return arr;
-        int maxnum = arr.back();
-        arr[n - 1] = -1;
-        for (int i = n - 2; i >= 0; --i) {
-            int tmpnum = maxnum;
-            maxnum = max(maxnum, arr[i]);
-            arr[i] = tmpnum;
+//          int n = arr.size();
+//         if (n == 0) return arr;
+//         int maxnum = arr.back();
+//         arr[n - 1] = -1;
+//         for (int i = n - 2; i >= 0; --i) {
+//             int tmpnum = maxnum;
+//             maxnum = max(maxnum, arr[i]);
+//             arr[i] = tmpnum;
+//         }
+//         return arr;
+        
+        
+        
+        
+         if (arr.size()==0) return arr;
+        if (arr.size()==1) { arr[arr.size()-1] = -1; return arr; }
+        size_t arrSize = arr.size(), i = arrSize-1, i_max = 0, l = 1;
+        while (l <= arrSize-1) {
+            while (i>=l) {
+                if (arr[i] > arr[i_max]) i_max = i;
+                i--;
+            }
+            for(i = l-1; i < i_max; i++) arr[i] = arr[i_max];
+            l = i_max + 1;
+            i_max = arrSize-1;
+            i = arrSize-1;
         }
+        arr[arrSize-1] = -1;
         return arr;
     }
 };
