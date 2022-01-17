f6e548869760f65e4a240ad9995bdc7d29f24148
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-17 20:20:35 +0530

    Update twoSum.cpp

--- arrays/twoSum.cpp
+++ arrays/twoSum.cpp
@@ -25,18 +25,58 @@ public:
       
       
         
-      vector<int> res;
-        for(auto it1=nums.begin();it1!=nums.end();++it1){     
-            auto it2=find(it1+1,nums.end(),target-*it1);
-            if(it2!=nums.end()){
-                res.push_back(it1-nums.begin());
-                res.push_back(it2-nums.begin());
-                break;
+//       vector<int> res;
+//         for(auto it1=nums.begin();it1!=nums.end();++it1){     
+//             auto it2=find(it1+1,nums.end(),target-*it1);
+//             if(it2!=nums.end()){
+//                 res.push_back(it1-nums.begin());
+//                 res.push_back(it2-nums.begin());
+//                 break;
+//             }
+//         }
+//         return res;
+      
+  
+        
+        
+        
+        
+        
+     
+       int start = 0, end = nums.size() - 1;
+        
+        vector< int > ans;
+        
+        vector< int > v;
+        v = nums;
+        sort(v.begin(), v.end());
+        
+        while(v[start] + v[end] != target)
+        {
+            if(v[start] + v[end] > target)
+                end-- ;
+            else if(v[start] + v[end] < target)
+                start ++ ;
+        }
+        
+       int st = 0, en = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(v[start] == nums[i] && st == 0)
+            {
+                ans.push_back(i);
+                st = 1;
+            }
+            else if(v[end] == nums[i] && en == 0)
+            {
+                ans.push_back(i);
+                en = 1;
             }
         }
-        return res;
-      
-      
+        return ans;
+   
+        
+        
     }  
       
 };
