cbea3b6b1d46a5a84d37a4c4dce68aba22c5efc3
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-23 18:34:17 +0530

    Create fibonacci.cpp

---
+++ arrays/fibonacci.cpp
@@ -0,0 +1,83 @@
+int fibonacciNumber(int n){
+    // Write your code here.
+//     int m = 1000000007;
+//  	  int a = 0, b = 1, c, i;
+//     if( n == 0)
+//         return a%m;
+//     for(i = 2; i <= n; i++)
+//     {
+//        c = (a + b)%m;
+//        a = b%m;
+//        b = c%m;
+//     }
+//     return b%m;
+    
+    
+    
+    
+    
+//     if (n <= 1) {
+//         return n%m;
+//     }
+//     return (fibonacciNumber(n - 1) + fibonacciNumber(n - 2))%m;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    int mod = (int) 1e9 + 7;
+//    int a = 0;
+//    int b = 1;
+//    int c;
+//    for (int i = 2; i <= n; i++) {
+//        // The variable c stores the ith Fibonacci Number.
+//        c = (a + b) % mod;
+//        // Updating values of a and b.
+//        a = b;
+//        b = c;
+//    }
+//    // The variable b stores the nth Fibonacci Number.
+//    return b;
+    
+    
+    
+    
+//     int M = 1000000007;
+//     if (n <= 1){
+//       	return n;
+//     }
+//     else{
+//    		return (fibonacciNumber(n-1)%M + fibonacciNumber(n-2)%M)%M;
+//     }
+    
+    
+    
+    
+   
+//    int i, fibonacciFirst = 0, fibonacciSecond = 1, fibonacciResult;
+//     int num =n;
+//     cout<<num;
+//     if(num == 0)
+//         return fibonacciFirst;
+//     else if(num == 1)
+//         return fibonacciSecond;
+//     else
+//         for(i=0; i<num; i++){
+//             fibonacciResult = fibonacciFirst + fibonacciSecond;
+//             fibonacciFirst = fibonacciSecond;
+//             fibonacciSecond = fibonacciResult;
+//         } 
+//     return fibonacciResult;
+    
+    
+    
+    
+
+    
+   
+}
