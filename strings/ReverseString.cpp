91292db2a2a5ab67d7c9bd1069692a5d4d304df4
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-27 18:25:09 +0530

    Update ReverseString.cpp

--- strings/ReverseString.cpp
+++ strings/ReverseString.cpp
@@ -12,3 +12,30 @@
             }
           
     }
+
+
+
+// https://www.codingninjas.com/codestudio/problems/reverse-the-string_799927
+
+
+
+void solve(int i, int j, string& str){
+    
+    if(i>j){
+        return;
+    }
+    
+    swap(str[i++],str[j--]);
+    
+    solve(i,j,str);
+    
+}
+
+string reverseString(string str)
+{
+	// Write your code here.
+    int i =0;
+    int j = str.length()-1;
+    solve(i,j,str);
+    return  str;
+}
