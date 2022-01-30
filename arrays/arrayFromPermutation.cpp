e2b700f3ea70ed9ac4749b64fda23078c3d5e8af
Sohil Khan <91256558+s0hi1khan@users.noreply.github.com>
2022-01-31 01:13:39 +0530

    Create arrayFromPermutation.cpp

---
+++ arrays/arrayFromPermutation.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+
+
+// O(N)   O(N)
+
+// class Solution {
+// public:
+//     vector<int> buildArray(vector<int>& nums) {        
+//        vector<int> ans;
+//         int n = nums.size();
+//        for(int i =0; i<n ; i++){
+//            ans.push_back(nums[nums[i]]);
+//        } 
+//         return ans;     
+//     }
+// };
+
+
+
+
+
+
+// O(N)      O(1)
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {        
+
+    
+        
+}
